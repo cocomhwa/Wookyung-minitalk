5fc88e44ce712bac89ff5871d0cdd2e147097c7d
cocomhwa <swk951031@gmail.com>
2023-01-15 14:30:16 +0900

    fix : fix minitalk_bonus.h
    fix name of header guard

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/12 19:13:38 by wooshin           #+#    #+#             */
-/*   Updated: 2023/01/13 23:56:01 by wooshin          ###   ########.fr       */
+/*   Created: 2023/01/15 14:29:00 by wooshin           #+#    #+#             */
+/*   Updated: 2023/01/15 14:29:27 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 
 # include "./library/ft_printf/ft_printf.h"
 # include <unistd.h>
